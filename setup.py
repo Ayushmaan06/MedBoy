1e4675703afab9d7890d5760fcb9a9040a185987
ayushmaan <s.ayushmaan06@gmail.com>
2025-03-07 21:17:19 +0530

    Done

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.1',
+    author= 'Ayushmaan06',
+    author_email= 's.ayushmaan06@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
